4e62b79548fccf40a69254b5d8277d8ee00463c8
anashussain93 <anas_hussain93@hotmail.com>
2018-01-04 19:10:17 +0500

    My Project Setup Completed successfully

---
+++ shopbackend/src/main/java/com/aks/shopbackend/App.java
@@ -0,0 +1,13 @@
+package com.aks.shopbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
